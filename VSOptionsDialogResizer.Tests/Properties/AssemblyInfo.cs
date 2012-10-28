54dd585551784d28ff97a3eb699a08902a59f3e7
Marcel Hoyer <mhoyer@pixelplastic.de>
2012-10-28 13:55:40 +0100

    added initial test project for VSOptionsDialogResizer

---
+++ VSOptionsDialogResizer.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VSOptionsDialogResizer.Tests")]
+[assembly: Guid("c58fb32e-0a76-466a-b135-1c72607949f9")]
