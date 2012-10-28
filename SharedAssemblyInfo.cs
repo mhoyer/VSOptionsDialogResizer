4a1b463261ba2260bc964dbabac671b200c6a673
Marcel Hoyer <mhoyer@pixelplastic.de>
2012-10-28 13:19:25 +0100

    extracted SharedAssemblyInfo.cs

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("An attempt to fix this stand of the 90s dialog in Visual Studio.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Marcel Hoyer")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0.0")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyKeyName("")]
--- VSOptionsDialogResizer.Addin/AssemblyInfo.cs
+++ VSOptionsDialogResizer.Addin/AssemblyInfo.cs
@@ -1,16 +1,3 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("VSOptionsDialogResizer.AddIn")]
-[assembly: AssemblyDescription("An attempt to fix this stand of the 90s dialog in Visual Studio.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Marcel Hoyer")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: AssemblyVersion("0.1.*")]
-
-[assembly: AssemblyDelaySign(false)]
-[assembly: AssemblyKeyFile("")]
-[assembly: AssemblyKeyName("")]
