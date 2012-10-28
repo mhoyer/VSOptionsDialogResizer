e5006e3c19f140d814e11854bb83ef238b90d362
Marcel Hoyer <mhoyer@pixelplastic.de>
2012-10-28 16:14:24 +0100

    added initial implementation of OptionsDialogWatcher.Listen method

---
+++ VSOptionsDialogResizer.Tests/OptionsDialogWatcher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Machine.Fakes;
+using Machine.Specifications;
+
+namespace VSOptionsDialogResizer.Tests
+{
+    public class when_listening_for_options_dialog_to_open : WithSubject<OptionsDialogWatcher>
+    {
+        Because of = () => Subject.Listen(new IntPtr(42));
+
+        It should_try_to_find_the_options_dialog = 
+            () => The<IOptionsDialogFinder>().WasToldTo(f => f.Find(new IntPtr(42)));
+
+        It should_try_to_find_the_options_dialog_multiple_times = 
+            () => The<IOptionsDialogFinder>().WasToldTo(f => f.Find(new IntPtr(42)));
+    }
+}
--- VSOptionsDialogResizer/OptionsDialogWatcher.cs
+++ VSOptionsDialogResizer/OptionsDialogWatcher.cs
@@ -4,9 +4,16 @@ namespace VSOptionsDialogResizer
 {
     public class OptionsDialogWatcher : IOptionsDialogWatcher
     {
+        readonly IOptionsDialogFinder _optionsDialogFinder;
+
+        public OptionsDialogWatcher(IOptionsDialogFinder optionsDialogFinder)
+        {
+            _optionsDialogFinder = optionsDialogFinder;
+        }
+
         public void Listen(IntPtr mainWindow)
         {
-            
+            _optionsDialogFinder.Find(mainWindow);
         }
     }
 }
