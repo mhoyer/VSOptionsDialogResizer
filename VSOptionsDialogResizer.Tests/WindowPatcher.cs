4765027bb4e0cbf6ce9a67144d232cbffd811f12
Marcel Hoyer <mhoyer@pixelplastic.de>
2012-10-31 13:17:14 +0100

    added WindowPatcher.ExecuteModifier method to invoke initialiy given window modifier

--- VSOptionsDialogResizer.Tests/WindowPatcher.cs
+++ VSOptionsDialogResizer.Tests/WindowPatcher.cs
@@ -11,4 +11,13 @@ namespace VSOptionsDialogResizer.Tests
         It should_start_the_cyclic_modifier_with_20ms_sleep =
             () => The<ICyclicWorker>().WasToldTo(w => w.Start(20, Param.IsAny<Action>()));
     }
+
+    public class when_executing_a_modifier : WithSubject<WindowPatcher>
+    {
+        Because of = () => Subject.ExecuteModifier(_optionsDialogWindow);
+
+        It should_execute_ = () => The<IWindowModifier>().WasToldTo(m => m.Modify(_optionsDialogWindow));
+
+        static readonly IntPtr _optionsDialogWindow = new IntPtr(2);
+    }
 }
--- VSOptionsDialogResizer/WindowPatcher.cs
+++ VSOptionsDialogResizer/WindowPatcher.cs
@@ -5,15 +5,22 @@ namespace VSOptionsDialogResizer
     public class WindowPatcher : IWindowPatcher
     {
         readonly ICyclicWorker _cyclicWorker;
+        readonly IWindowModifier _modifier;
 
-        public WindowPatcher(ICyclicWorker cyclicWorker)
+        public WindowPatcher(ICyclicWorker cyclicWorker, IWindowModifier modifier)
         {
             _cyclicWorker = cyclicWorker;
+            _modifier = modifier;
         }
 
         public void PatchUntilClose(IntPtr window)
         {
             _cyclicWorker.Start(20, () => {});
+        }
+
+        public void ExecuteModifier(IntPtr window)
+        {
+            _modifier.Modify(window);
         }
     }
 }
