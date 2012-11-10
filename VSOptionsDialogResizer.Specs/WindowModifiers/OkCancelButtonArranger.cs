24d0f4309cf6e98fc3d8f88c040bf208ac9c7b59
Marcel Hoyer <mhoyer@pixelplastic.de>
2012-11-10 22:04:51 +0100

    made OkCancelButtonArranger determine the position of found ok button

--- VSOptionsDialogResizer.Specs/WindowModifiers/OkCancelButtonArranger.cs
+++ VSOptionsDialogResizer.Specs/WindowModifiers/OkCancelButtonArranger.cs
@@ -14,6 +14,10 @@ namespace VSOptionsDialogResizer.Specs.WindowModifiers
                 The<IPInvoker>()
                     .WhenToldTo(p => p.FindAllChildrenByClassName(_optionsWindow, "Button"))
                     .Return(new[] { _ok });
+
+                The<IPInvoker>()
+                    .WhenToldTo(p => p.GetWindowText(_ok))
+                    .Return("OK");
             };
 
         Because of = () => Subject.Modify(_optionsWindow, 400, 200);
@@ -23,6 +27,9 @@ namespace VSOptionsDialogResizer.Specs.WindowModifiers
 
         It should_determine_ok_button =
             () => The<IPInvoker>().WasToldTo(p => p.GetWindowText(_ok));
+
+        It should_determine_current_position_of_ok_button =
+            () => The<IPInvoker>().WasToldTo(p => p.GetWindowRect(_ok));
 
         static readonly IntPtr _optionsWindow = new IntPtr(1);
         static readonly IntPtr _ok = new IntPtr(2);
@@ -40,7 +47,8 @@ namespace VSOptionsDialogResizer.Specs.WindowModifiers
         public void Modify(IntPtr window, uint width, uint height)
         {
             var button = _pInvoker.FindAllChildrenByClassName(window, "Button").First();
-            _pInvoker.GetWindowText(button);
+            if (_pInvoker.GetWindowText(button) != "OK") return;
+            _pInvoker.GetWindowRect(button);
         }
     }
 }
