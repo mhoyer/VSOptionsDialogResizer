9cba9f6c223141499ed8aed38612cf2f687989fc
Marcel Hoyer <mhoyer@pixelplastic.de>
2012-11-10 20:58:55 +0100

    finished implementation of MainTreeResizer

--- VSOptionsDialogResizer.Specs/WindowModifiers/MainTreeResizer.cs
+++ VSOptionsDialogResizer.Specs/WindowModifiers/MainTreeResizer.cs
@@ -17,8 +17,8 @@ namespace VSOptionsDialogResizer.Specs.WindowModifiers
 
                 The<IPInvoker>()
                     .WhenToldTo(p => p.GetWindowRect(_tree))
-                    .Return(new Rect{ X1 = 10, Y1 = 10,
-                                      X2 = 250, Y2 = 500});
+                    .Return(new Rect{ X1 = 200, Y1 = 200,
+                                      X2 = 500, Y2 = 800});
             };
 
         Because of = () => Subject.Modify(_optionsWindow, 400, 200);
@@ -31,10 +31,34 @@ namespace VSOptionsDialogResizer.Specs.WindowModifiers
 
         It should_move_the_tree_but_keep_the_x_offset =
             () => The<IPInvoker>().WasToldTo(p => p.MoveWindow(_tree,
-                                                               10, // = X1
+                                                               10, // = CONST(10)
                                                                Param.IsAny<int>(),
                                                                Param.IsAny<uint>(),
                                                                Param.IsAny<uint>(),
+                                                               true));
+
+        It should_move_the_tree_but_keep_the_y_offset =
+            () => The<IPInvoker>().WasToldTo(p => p.MoveWindow(_tree,
+                                                               Param.IsAny<int>(),
+                                                               10, // = CONST(10)
+                                                               Param.IsAny<uint>(),
+                                                               Param.IsAny<uint>(),
+                                                               true));
+
+        It should_move_the_tree_but_keep_the_width =
+            () => The<IPInvoker>().WasToldTo(p => p.MoveWindow(_tree,
+                                                               Param.IsAny<int>(),
+                                                               Param.IsAny<int>(),
+                                                               300, // = X2-X1
+                                                               Param.IsAny<uint>(),
+                                                               true));
+
+        It should_move_the_tree_with_the_fixed_the_height =
+            () => The<IPInvoker>().WasToldTo(p => p.MoveWindow(_tree,
+                                                               Param.IsAny<int>(),
+                                                               Param.IsAny<int>(),
+                                                               Param.IsAny<uint>(),
+                                                               180, // = newHeightOfOptionsDialog - 20
                                                                true));
 
         static readonly IntPtr _optionsWindow = new IntPtr(1);
@@ -56,7 +80,11 @@ namespace VSOptionsDialogResizer.Specs.WindowModifiers
             if (tree == IntPtr.Zero) return;
 
             var treeRect = _pInvoker.GetWindowRect(tree);
-            _pInvoker.MoveWindow(tree, treeRect.X1, 0, 0, 0, true);
+            _pInvoker.MoveWindow(tree, 
+                10, 
+                10, 
+                treeRect.Width,
+                height - 20, true);
         }
     }
 }
