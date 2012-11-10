c2cf5c1b88b432bcbeac72a25eca779fbc24dbd8
Marcel Hoyer <mhoyer@pixelplastic.de>
2012-11-10 12:46:57 +0100

    added new in-thread CyclicWorker

---
+++ VSOptionsDialogResizer.Tests/CyclicWorker.cs
@@ -0,0 +1,30 @@
+﻿using Machine.Fakes;
+using Machine.Specifications;
+
+namespace VSOptionsDialogResizer.Tests
+{
+    public class when_starting_a_cyclic_action : WithCyclicWorker
+    {
+        Because of = () => Subject.Start(SLEEP, () => Triggered = true);
+
+        It should_have_triggered_the_action_at_least_once = () => Triggered.ShouldBeTrue();
+    }
+    
+    public class when_running_a_cyclic_worker_again : WithCyclicWorker
+    {
+        Establish context = () => Subject.Start(SLEEP, () => { }); // trigger first job with noop
+
+        Because of_starting_a_second_time = () => Subject.Start(SLEEP, () => Triggered = true);
+
+        It should_have_allow_starting_the_action_again = () => Triggered.ShouldBeTrue();
+    }
+
+    public class WithCyclicWorker : WithFakes
+    {
+        Establish context = () => Subject = new CyclicWorker(stopAction: () => true);
+
+        protected static bool Triggered;
+        protected static CyclicWorker Subject;
+        protected const int SLEEP = 5;
+    }
+}
---
+++ VSOptionsDialogResizer/CyclicWorker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace VSOptionsDialogResizer
+{
+    public class CyclicWorker : ICyclicWorker
+    {
+        readonly Func<bool> _stopAction;
+
+        public CyclicWorker(Func<bool> stopAction)
+        {
+            _stopAction = stopAction;
+        }
+
+        public void Start(int sleep, Action action)
+        {
+            while (true)
+            {
+                action.Invoke();
+                if (_stopAction.Invoke()) break;
+                Thread.Sleep(sleep);
+            }
+        }
+    }
+
+}
